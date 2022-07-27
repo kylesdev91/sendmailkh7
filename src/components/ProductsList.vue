9a1cb8bf974851941a64ccc35cc644591f8c4b62
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-27 12:10:08 -0500

    before total

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <button @click="sendEmail">Email</button>
+    <div>
+      <a v-if="userInfo" :href="`/.auth/logout`"><button>Logout</button></a>
+      <a v-if="!userInfo" :href="`/.auth/login/aad`"><button>Login</button></a>
+      <div class="user" v-if="userInfo">
+        <p>Welcome</p>
+        <p>{{ userInfo.userDetails }}</p>
+      </div>
+    </div>
   </div>
   <h2>{{ product.id }}</h2>
   <h2>{{ product.name }}</h2>
@@ -13,11 +21,25 @@ export default {
   data() {
     return {
       items: items,
+      userInfo: {
+        type: Object,
+        default() {},
+      },
     };
   },
+  computed: {
+    products() {
+      return this.$store.getters.cartItems;
+    },
+  },
+
   props: ['product'],
+  async created() {
+    this.userInfo = await this.getUserInfo();
+  },
   methods: {
     sendEmail() {
+      console.log(this.userInfo.userDetails);
       var content = this.items.reduce(function (a, b) {
         return a + '<tr><td>' + b.id + '</a></td><td>' + b.name + '</td></tr>';
       }, '');
@@ -25,10 +47,25 @@ export default {
         emailSubject: 'Online Order',
         emailBody: content,
         orderTotal: 10,
+        emailAddress: this.userInfo.userDetails,
       };
-      axios.post('/api/sendmail2', formData).then((response) => {
+      axios.post('/api/sendmail7', formData).then((response) => {
         console.log(response);
       });
+    },
+    login() {
+      this.$router.push('/.auth/login/aad');
+    },
+    async getUserInfo() {
+      try {
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        return clientPrincipal;
+      } catch (error) {
+        console.error('No profile could be found');
+        return undefined;
+      }
     },
   },
 };
